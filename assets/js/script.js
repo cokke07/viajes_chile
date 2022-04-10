9d908307e60c95ec46f5bbc765d34adc373bac20
cokke07 <coke.jgomez@gmail.com>
2022-04-10 15:17:33 -0400

    Se agregan eventos con js, alert, tooltips y se finaliza la pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 //declararemos eventos y metodos Jquery
 $(document).ready(function () {
 
+//contar y mostrar la cantidad de caracteres ingresados en el textarea del formulario de contacto
+$('textarea').keyup(updateCount);
+$('textarea').keydown(updateCount);
 
+function updateCount() {
+    $('#cont2').text($(this).val().length);
+}
+
+// evento en el boton de Enviar correo
+ $("#enviarMensaje").click(function () {
+
+    alert("Tu mensaje a sido recibido correctamente.")
+        
+});
 
 });
